a24c8f3732b697373c46d07f855d3de439819729
dksgudwn <dksgudwn620@gmail.com>
2022-06-16 19:18:17 +0900

    d

--- FlightShooting/Assets/02_Scripts/EnemySpawner.cs
+++ FlightShooting/Assets/02_Scripts/EnemySpawner.cs
@@ -27,9 +27,4 @@ public class EnemySpawner : MonoBehaviour
             yield return new WaitForSeconds(spawnTime);
         }
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
